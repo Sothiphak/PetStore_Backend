8789bb56a13a36c1c4c0e93d50f3aa1c5b2c59c5
Sothiphak <sopheapsothiphak@gmail.com>
2026-01-23 17:46:52 +0700

    Update bakong payment with security intergrated

--- services/paymentService.js
+++ services/paymentService.js
@@ -5,13 +5,13 @@ const axios = require('axios');
 
 class PaymentService {
     constructor() {
-        this.merchantId = process.env.BAKONG_MERCHANT_ID || "khqr@aclb";
-        this.merchantName = process.env.BAKONG_MERCHANT_NAME || "PetStore+";
+        this.merchantId = "khqr@aclb";
+        this.merchantName = "PetStore+";
         this.merchantCity = "Phnom Penh";
-        this.acquiringBank = process.env.BAKONG_ACQUIRING_BANK || "Acleda Bank"; 
+        this.acquiringBank = "Acleda Bank"; 
         
         // Keep Token
-        this.bakongToken = process.env.BAKONG_API_TOKEN || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7ImlkIjoiMWJkMDEzZTRlNDExNGE0YSJ9LCJpYXQiOjE3NjkxNTc3NTQsImV4cCI6MTc3NjkzMzc1NH0.1lh20A_epTUhJPWFu15yq_CqZ6WbeL2XhV0Z-dclNCo";
+        this.bakongToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7ImlkIjoiMWJkMDEzZTRlNDExNGE0YSJ9LCJpYXQiOjE3NjkxNTc3NTQsImV4cCI6MTc3NjkzMzc1NH0.1lh20A_epTUhJPWFu15yq_CqZ6WbeL2XhV0Z-dclNCo";
         this.bakongApiUrl = "https://api-bakong.nbc.gov.kh/v1/check_transaction_by_md5";
     }
 
