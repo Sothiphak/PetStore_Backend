ec39003e125296600c91bc59fb4838f71371260d
Sothiphak <sopheapsothiphak@gmail.com>
2026-01-26 17:12:02 +0700

    fix: restore original working email config

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,28 +1,23 @@
-// utils/sendEmail.js - Make sure timeout is set
 const nodemailer = require('nodemailer');
 
 const sendEmail = async (options) => {
   const transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com',
-    port: 587,
-    secure: false, // Use TLS
+    service: 'gmail',
     auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
+      user: process.env.EMAIL_USER, 
+      pass: process.env.EMAIL_PASS, 
     },
-    connectionTimeout: 30000, // 30 seconds
-    greetingTimeout: 30000,
-    socketTimeout: 30000,
   });
 
   const mailOptions = {
-    from: `PetStore+ <${process.env.EMAIL_USER}>`,
+    from: `"PetStore+ Support" <${process.env.EMAIL_USER}>`,
     to: options.email,
     subject: options.subject,
     html: options.message,
   };
 
-  await transporter.sendMail(mailOptions);
+  const info = await transporter.sendMail(mailOptions);
+  console.log("📧 Real Email Sent: %s", info.messageId);
 };
 
 module.exports = sendEmail;
